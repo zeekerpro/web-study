ed98ad4ac4f45a624ba74030a5bdc23c95e8cfb3
zeekerpro <zeekerpro@gmail.com>
2018-01-27 21:02:57 -0600

    c3p0utils 添加事务管理逻辑

--- src/main/java/com/zeeker/utils/dbutils/common/c3p0/C3p0Utils.java
+++ src/main/java/com/zeeker/utils/dbutils/common/c3p0/C3p0Utils.java
@@ -15,6 +15,9 @@ import java.sql.SQLException;
 public class C3p0Utils {
     private  static  ComboPooledDataSource cpds;
 
+    // 使用 threadlocal 实现事务管理
+    private  static ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
+
     static {
         cpds = new ComboPooledDataSource("mysql");
     }
@@ -28,19 +31,76 @@ public class C3p0Utils {
     }
 
     /**
-     * 从连接池中获取一条连接
+     * 获取用于事务处理的链接
      * @return
      */
-    public Connection getConnection(){
-        Connection connection = null;
+    public static Connection getConnection(){
+        Connection connection = threadLocal.get();
         try {
-            connection = cpds.getConnection();
+            if (connection == null){
+                connection = cpds.getConnection();
+                threadLocal.set(connection);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return connection;
     }
 
-    
+
+    /**
+     * 在当前线程开启事务
+     */
+    public static void startTransaction(){
+        // 得到当前线程上绑定的连接，开启事务
+        Connection connection = threadLocal.get();
+        // 当前线程没绑定，手动获取连接，并绑定到线程
+        if (connection == null){
+            connection = getConnection();
+        }
+        // 开启事务
+        try {
+            connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 提交事务
+     */
+    public  static void commitTransaction(){
+        Connection connection = threadLocal.get();
+        // 连接不为 null 才提交， 否则不提交
+        if (connection != null){
+            try {
+                connection.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 关闭事务
+     */
+    public static void closeTranscation(){
+        Connection connection = threadLocal.get();
+
+        try {
+            if (connection != null) {
+                connection.setAutoCommit(true);
+                connection.close();
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            // 从当前线程中移除绑定的链接，一定要放在 finally语句中
+            threadLocal.remove();
+        }
+
+    }
+
+
 
 }
---
+++ src/main/test/utils/ClassTest.java
@@ -0,0 +1,21 @@
+/**
+ * @fileName :     ClassTest
+ * @author :       zeeker
+ * @date :         1/27/18 8:54 PM
+ * @description :
+ */
+
+package utils;
+
+import org.junit.Test;
+
+public class ClassTest {
+    @Test
+    public void threadLodalTest(){
+        ThreadLocal<Integer>  threadLocal = new ThreadLocal<>();
+        threadLocal.set(90);
+        threadLocal.set(80);
+        System.out.println(threadLocal.get());
+        System.out.println(threadLocal.get());
+    }
+}
