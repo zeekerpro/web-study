7f9dd613b7f0fa78d064edbc89d951a423e2fe87
zeekerpro <zeekerpro@gmail.com>
2018-01-28 21:20:13 -0600

    上传文件中文文件名乱码

--- src/main/java/com/zeeker/keychain/api/FileUpload.java
+++ src/main/java/com/zeeker/keychain/api/FileUpload.java
@@ -23,46 +23,61 @@ import java.util.List;
 public class FileUpload extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String savePath = this.getServletContext().getRealPath("/WEB-INF/upload");
+
+        // 中文乱码
+        req.setCharacterEncoding("utf-8");
+
+        File file = new File(savePath);
+        if (!file.exists()){
+            file.mkdir();
+        }
         try{
             // 1. 获取解析工厂
             DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
             // 2. 获取解析器
             ServletFileUpload upload = new ServletFileUpload(diskFileItemFactory);
-            // 3. 解析request
-            List<FileItem> fileItemList = upload.parseRequest(req);
-            // 4. 迭代解析
-            for (FileItem fileItem : fileItemList){
+            // upload.setHeaderEncoding("utf-8");
+            // 判断删除的数据是不是上传表单数据
+            if (ServletFileUpload.isMultipartContent(req)) {
+                // 3. 解析request
+                List<FileItem> fileItemList = upload.parseRequest(req);
+                // 4. 迭代解析
+                for (FileItem fileItem : fileItemList) {
 
-                if (fileItem.isFormField()){
-                    // 普通输入项
-                    String paramName = fileItem.getName();
-                    String paramValue = fileItem.getString();
-                }else {
-                    // 上传的文件
-                    String fileName = fileItem.getName().substring(fileItem.getName().lastIndexOf(File.pathSeparatorChar) + 1);
-                    InputStream inputStream = null;
-                    OutputStream outputStream = null;
-                    try {
-                            inputStream =req.getInputStream();
+                    if (fileItem.isFormField()) {
+                        // 普通输入项
+                        String paramName = fileItem.getName();
+                        String paramValue = fileItem.getString("UTF-8");
+                    } else {
+                        // 上传的文件
+                        String fileName = fileItem.getName().substring(fileItem.getName().lastIndexOf(File.pathSeparatorChar) + 1);
+                        InputStream inputStream = null;
+                        OutputStream outputStream = new FileOutputStream(savePath + File.separator + fileName);
+                        try {
+                            inputStream = fileItem.getInputStream();
+                            int len = 0;
+                            byte[] buffer = new byte[1024];
+                            while ((len = inputStream.read(buffer)) > 0) {
+                                outputStream.write(buffer);
+                            }
+                        } finally {
+                            if (inputStream != null) {
+                                try {
+                                    inputStream.close();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            if (outputStream != null) {
+                                try {
+                                    outputStream.close();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
 
-                        int len = 0;
-                        byte[] buffer = new byte[1024];
-                        outputStream = new FileOutputStream("./upload/");
-                        while ((len = inputStream.read(buffer)) > 0) {
-                            outputStream.write(buffer);
                         }
-                    }finally {
-                        if (inputStream != null) {
-                            try {
-                                inputStream.close();
-                            }catch (Exception e){e.printStackTrace();}
-                        }
-                        if (outputStream != null){
-                            try {
-                                outputStream.close();
-                            }catch (Exception e){e.printStackTrace();}
-                        }
-
                     }
                 }
             }
--- src/main/webapp/upload.jsp
+++ src/main/webapp/upload.jsp
@@ -15,6 +15,7 @@
 
     <form action="${pageContext.request.contextPath}/upload" enctype="multipart/form-data" method="post">
         <input type="file" name="file" />
+        <input type="submit" value="提交"/>
     </form>
 
 </body>
